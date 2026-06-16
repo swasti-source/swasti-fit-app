4ffbb38fe2bf889396f1e65565caa6cc03e84102
swasti-source <swasti@sparxstudio.com>
2026-06-16 21:47:08 +0530

    Add Swasti Fit PWA

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'swasti-fit-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './swasti_app_icons/icon-192.png',
+  './swasti_app_icons/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (e.request.method === 'GET' && res.ok) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => cached))
+  );
+});
